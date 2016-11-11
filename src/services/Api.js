0143749c96a402b3a5c0c17b9aaa310442e4ea26
Genaro Rocha <genarorg@gmail.com>
2016-11-10 17:50:49 -0800

    Add docs for Api.js

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,5 +1,10 @@
+/**
+ * The search Api. This follows the module pattern and exports
+ * an object with a `search` method.
+ */
 export default (function Api() {
 
+  // Our data set. In a private function.
   const _getCities = function() {
     return ['san', 'san jose', 'santiago', 'san francisco', 
             'santa rosa', 'san juan', 'sabadell', 'salamanca', 
@@ -9,6 +14,12 @@ export default (function Api() {
             'tokyo', 'tulsa'];
   };
 
+  /**
+   * This method will take a term as a string and will match it against
+   * the cities dataset.
+   * @param {string} term - The search term.
+   * @return {array} - The search results. Null if term is null or empty.
+   */
   const search = function(term) {
     // Get an array of results.
     if (term) {
